4b650e7bc60840fb3a58941da7705f09b33e4be7
claudiahW <murungiclaud@gmail.com>
2024-01-31 23:44:27 +0300

    create an input box and a submit button

---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+export default function Todo() {
+    return (
+
+     <div>
+        <form>
+            <input type="text" />
+            <button type="submit">Add</button>
+        </form>
+    </div>
+    );
+}
