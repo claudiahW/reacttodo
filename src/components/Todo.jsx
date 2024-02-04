73c763dfdb56400b5d444a72baadb134490f888f
claudiahW <murungiclaud@gmail.com>
2024-02-04 19:16:19 +0300

    write a function that allows the user to add to the list and display the items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 export default function Todo() {
     const [todo, setTodo] = useState("");
+    const [todos,setTodos] = useState([]);  
     function handleSubmit(e){
       e.preventDefault();
+      setTodos([...todos,todo]);
+      setTodo("");
+
     }
     return (
      <div>
@@ -15,6 +19,9 @@ export default function Todo() {
             />
             <button type="submit">Add</button>
         </form>
+      {todos.map((item) => (
+      <h3>{item}</h3>
+      ))}  
     </div>
     );
 }
