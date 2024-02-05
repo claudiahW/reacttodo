9cf04656f7fb47516a4ccafad70c9cc663a7ee79
claudiahW <murungiclaud@gmail.com>
2024-02-05 08:07:18 +0300

    create a component for css styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import styles from "./header.module.css";
 export default function Header() {
+
     return (
-        <div>My Todo List</div>
+        <div className={styles.header}>My Todo List</div>
     );
 }
