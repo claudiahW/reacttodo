e2467f64c35036bcd7461b11529810c601646389
claudiahW <murungiclaud@gmail.com>
2024-02-05 12:49:19 +0300

    add a complete task functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,21 @@
 import styles from "./todoitem.module.css";
-export default function TodoItem({item}) {
+export default function TodoItem({item, todos, setTodos }) {
+    function handleDelete(item){
+        setTodos(todos.filter((todo) => todo !== item));
+    
+    }
     return  (
     <div className={styles.item}>
-        <div className={styles.itemName}>{item}
+        <div className={styles.itemName}>
+              {item.name}
          <span>
-            <button className={styles.deleteButton}>
+            <button 
+            onClick={() => handleDelete(item)} 
+            className={styles.deleteButton}>
                x 
             </button>
-        </span></div>
+        </span>
+        </div>
        
         <hr className={styles.line} />
     </div>
